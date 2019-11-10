9cfbe328dc5dd5e17377baf64c8b3c51e2e97315
randomtable <alefiori3@gmail.com>
2019-11-11 00:19:12 +0100

    Add files via upload

---
+++ XabalTest/XabalTest/Form1.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(260, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Funzione di check"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 67)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(260, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Invia Stringa a Xabal"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 41)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(260, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 96)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(260, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 122)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(260, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Invia Operazione"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 154)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Testo"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(52, 151)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(220, 20)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 180)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Chiave"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(58, 177)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(214, 20)
+        Me.TextBox4.TabIndex = 8
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 203)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(260, 23)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "Cifratura"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(12, 232)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(260, 23)
+        Me.Button5.TabIndex = 10
+        Me.Button5.Text = "Decifrazione"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(12, 261)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(260, 20)
+        Me.TextBox5.TabIndex = 11
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(12, 287)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(260, 23)
+        Me.Button6.TabIndex = 12
+        Me.Button6.Text = "Invia Codice"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(12, 316)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(260, 20)
+        Me.TextBox6.TabIndex = 13
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(12, 342)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(260, 23)
+        Me.Button7.TabIndex = 14
+        Me.Button7.Text = "Sanifica Stringa"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 411)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+
+End Class
---
+++ XabalTest/XabalTest/Form1.vb
@@ -0,0 +1,163 @@
+﻿Imports System.Net
+Imports System.Text
+Imports System.IO
+Imports System.Web
+
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim s As HttpWebRequest
+        Dim enc As UTF8Encoding
+        Dim postdata As String
+        Dim postdatabytes As Byte()
+        s = HttpWebRequest.Create("https://loremitalia.altervista.org/xabal.php")
+        enc = New System.Text.UTF8Encoding()
+        postdata = "function=test"
+        postdatabytes = enc.GetBytes(postdata)
+        s.Method = "POST"
+        s.ContentType = "application/x-www-form-urlencoded"
+        s.ContentLength = postdatabytes.Length
+
+        Using stream = s.GetRequestStream()
+            stream.Write(postdatabytes, 0, postdatabytes.Length)
+        End Using
+        Dim result = s.GetResponse()
+        Dim reader As New StreamReader(result.GetResponseStream())
+        Dim streamText As String = reader.ReadToEnd()
+        MsgBox(streamText)
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim s As HttpWebRequest
+        Dim enc As UTF8Encoding
+        Dim postdata As String
+        Dim postdatabytes As Byte()
+        s = HttpWebRequest.Create("https://loremitalia.altervista.org/xabal.php")
+        enc = New System.Text.UTF8Encoding()
+        postdata = "function=string&string=" & TextBox1.Text
+        postdatabytes = enc.GetBytes(postdata)
+        s.Method = "POST"
+        s.ContentType = "application/x-www-form-urlencoded"
+        s.ContentLength = postdatabytes.Length
+
+        Using stream = s.GetRequestStream()
+            stream.Write(postdatabytes, 0, postdatabytes.Length)
+        End Using
+        Dim result = s.GetResponse()
+        Dim reader As New StreamReader(result.GetResponseStream())
+        Dim streamText As String = reader.ReadToEnd()
+        MsgBox(streamText)
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim s As HttpWebRequest
+        Dim enc As UTF8Encoding
+        Dim postdata As String
+        Dim postdatabytes As Byte()
+        s = HttpWebRequest.Create("https://loremitalia.altervista.org/xabal.php")
+        enc = New System.Text.UTF8Encoding()
+        postdata = "function=operations&operation=" & System.Web.HttpUtility.UrlEncode(TextBox2.Text)
+        postdatabytes = enc.GetBytes(postdata)
+        s.Method = "POST"
+        s.ContentType = "application/x-www-form-urlencoded"
+        s.ContentLength = postdatabytes.Length
+
+        Using stream = s.GetRequestStream()
+            stream.Write(postdatabytes, 0, postdatabytes.Length)
+        End Using
+        Dim result = s.GetResponse()
+        Dim reader As New StreamReader(result.GetResponseStream())
+        Dim streamText As String = reader.ReadToEnd()
+        MsgBox(streamText)
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Dim s As HttpWebRequest
+        Dim enc As UTF8Encoding
+        Dim postdata As String
+        Dim postdatabytes As Byte()
+        s = HttpWebRequest.Create("https://loremitalia.altervista.org/xabal.php")
+        enc = New System.Text.UTF8Encoding()
+        postdata = "function=encrypt&testo=" & TextBox3.Text & "&chiave=" & TextBox4.Text
+        postdatabytes = enc.GetBytes(postdata)
+        s.Method = "POST"
+        s.ContentType = "application/x-www-form-urlencoded"
+        s.ContentLength = postdatabytes.Length
+
+        Using stream = s.GetRequestStream()
+            stream.Write(postdatabytes, 0, postdatabytes.Length)
+        End Using
+        Dim result = s.GetResponse()
+        Dim reader As New StreamReader(result.GetResponseStream())
+        Dim streamText As String = reader.ReadToEnd()
+        My.Computer.Clipboard.SetText(streamText)
+        MsgBox(streamText)
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Dim s As HttpWebRequest
+        Dim enc As UTF8Encoding
+        Dim postdata As String
+        Dim postdatabytes As Byte()
+        s = HttpWebRequest.Create("https://loremitalia.altervista.org/xabal.php")
+        enc = New System.Text.UTF8Encoding()
+        postdata = "function=decrypt&testo=" & TextBox3.Text & "&chiave=" & TextBox4.Text
+        postdatabytes = enc.GetBytes(postdata)
+        s.Method = "POST"
+        s.ContentType = "application/x-www-form-urlencoded"
+        s.ContentLength = postdatabytes.Length
+
+        Using stream = s.GetRequestStream()
+            stream.Write(postdatabytes, 0, postdatabytes.Length)
+        End Using
+        Dim result = s.GetResponse()
+        Dim reader As New StreamReader(result.GetResponseStream())
+        Dim streamText As String = reader.ReadToEnd()
+        My.Computer.Clipboard.SetText(streamText)
+        MsgBox(streamText)
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        Dim s As HttpWebRequest
+        Dim enc As UTF8Encoding
+        Dim postdata As String
+        Dim postdatabytes As Byte()
+        s = HttpWebRequest.Create("https://loremitalia.altervista.org/xabal.php")
+        enc = New System.Text.UTF8Encoding()
+        postdata = "function=code&code=" & System.Web.HttpUtility.UrlEncode(TextBox5.Text)
+        postdatabytes = enc.GetBytes(postdata)
+        s.Method = "POST"
+        s.ContentType = "application/x-www-form-urlencoded"
+        s.ContentLength = postdatabytes.Length
+
+        Using stream = s.GetRequestStream()
+            stream.Write(postdatabytes, 0, postdatabytes.Length)
+        End Using
+        Dim result = s.GetResponse()
+        Dim reader As New StreamReader(result.GetResponseStream())
+        Dim streamText As String = reader.ReadToEnd()
+        MsgBox(streamText)
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        Dim s As HttpWebRequest
+        Dim enc As UTF8Encoding
+        Dim postdata As String
+        Dim postdatabytes As Byte()
+        s = HttpWebRequest.Create("https://loremitalia.altervista.org/xabal.php")
+        enc = New System.Text.UTF8Encoding()
+        postdata = "function=sanitize&string=" & TextBox6.Text
+        postdatabytes = enc.GetBytes(postdata)
+        s.Method = "POST"
+        s.ContentType = "application/x-www-form-urlencoded"
+        s.ContentLength = postdatabytes.Length
+
+        Using stream = s.GetRequestStream()
+            stream.Write(postdatabytes, 0, postdatabytes.Length)
+        End Using
+        Dim result = s.GetResponse()
+        Dim reader As New StreamReader(result.GetResponseStream())
+        Dim streamText As String = reader.ReadToEnd()
+        MsgBox(streamText)
+    End Sub
+End Class
